ab664eee0c1562b928eb6f8504c52d99b14866f9
Hemanth Kapila <skapila@microsoft.com>
2015-05-26 15:57:32 +0530

    added examples for C# Interop

---
+++ gen/QuarryReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Check
+{
+    public class QuarryReader : IDisposable , IEnumerable<Slab>
+    {
+        private  IntPtr qreader;
+        public QuarryReader(string fileName, int i)
+        {
+            qreader = Interop.quarry_newReader(fileName, i);
+        }
+
+
+        public void Dispose()
+        {
+            Interop.quarry_closeReader(qreader);
+        }
+
+        public IEnumerator<Slab> GetEnumerator()
+        {
+            while (true) {
+                var slabPtr = Interop.quarry_read(qreader);
+                LocalSlab slab = new LocalSlab();
+
+                Marshal.PtrToStructure(slabPtr, slab);
+                var slabType = (SlabType)slab.slabType;
+                if (slabType == SlabType.EOF)
+                    break;
+                byte[] bytes = new byte[slab.slabTokenLen];
+                Marshal.Copy(slab.content, bytes, 0, slab.slabTokenLen);
+                var str = Encoding.UTF8.GetString(bytes);
+                var c = new Slab()
+                {
+                    Line = slab.slabLine,
+                    Column = slab.slabCol,
+                    Text = str,
+                    slabType = slabType,
+                    Metadata = slab.slabMD
+                };
+                Interop.quarry_freeSlab(slabPtr);
+                yield return c;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ gen/Slabs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Check
+{
+    public enum SlabType
+    {
+        Error,
+        Keyword,
+        Identifier,
+        STRING,
+        CHAR,
+        Numbers,
+        Operator,
+        Grouping,
+        Punctuation,
+        Comment,
+        Whitespace,
+        NewLine,
+        MetaId,
+        EOF
+    }
+    
+    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
+    class LocalSlab
+    {
+        internal int slabLine;
+        internal int slabCol;
+        internal int slabTokenLen;
+        internal int slabType;
+        internal IntPtr content;
+        internal UInt32 slabMD;
+    }
+    public class Slab
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+        public string Text { get; set; }
+        public SlabType slabType { get; set; }
+        public UInt32 Metadata { get; set; }
+
+        public override string ToString()
+        {
+            var text = new StringBuilder("{[");
+            text.Append(this.Line);
+            text.Append(',');
+            text.Append(this.Column);
+            text.Append(']');
+            text.Append(Text);
+            text.Append(" ::");
+            text.Append(this.slabType);
+            text.Append(" with ");
+            text.Append(this.Metadata);
+            text.Append("}");
+            return text.ToString();
+        }
+    }
+}
